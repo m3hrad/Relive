a16f9dd37914599cf9cafc9fe6651e9b7c8faaaa
m3hrad <mehrad.mohammadi@futurice.com>
2018-02-10 15:39:37 +0200

    tabs styles updated

--- App.js
+++ App.js
@@ -71,7 +71,6 @@ export default TabNavigator(
             style: {
                 backgroundColor: 'white',
                 borderTopColor: "transparent"
-
             },
         },
         animationEnabled: true,
